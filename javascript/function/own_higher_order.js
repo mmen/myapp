f241f8e657fa6387c4dde758d836e1f86e42c028
Molin Men <menmolin@yahoo.com>
2019-04-29 12:38:53 -0400

    added own higher order function

---
+++ javascript/function/own_higher_order.js
@@ -0,0 +1,20 @@
+/*
+Let’s say, we have an array of strings and we want to convert this array to an array of integers, where each element represent the length of the string in the original array.
+*/
+
+const strArray = ['JavaScript', 'Python', 'PHP', 'Java', 'C'];
+
+function mapForEach(arr, fn) {
+  const newArray = [];
+  for(let i = 0; i < arr.length; i++) {
+    newArray.push(
+      fn(arr[i])
+    );
+  }
+  return newArray;
+}
+const lenArray = mapForEach(strArray, function(item) {
+  return item.length;
+});
+// prints [ 10, 6, 3, 4, 1 ]
+console.log(lenArray);
