d8eebbaf11159568cbc0f59551ba11505c5cad5d
Molin Men <menmolin@yahoo.com>
2019-05-01 14:55:13 -0400

    added some projects and demos download from https://phpgurukul.com

--- php/pdo-examples/includes/conn.inc.php
+++ php/pdo-examples/includes/conn.inc.php
@@ -8,7 +8,18 @@ $charset = 'utf8mb4';
 
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
 
-$pdo = new PDO($dsn, $user, $pass);
+//$pdo = new PDO($dsn, $user, $pass);
+
+try {
+	
+	$pdo = new PDO($dsn, $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	
+} catch(PDOException $e) {
+    
+	echo 'ERROR: ' . $e->getMessage();
+
+}
 
 /*
 $options = [
