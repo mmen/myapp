746169b255d9be9a17d04824f2a0f135dba9a5d6
Molin Men <molin.men@etsmtl.ca>
2021-10-08 13:20:15 -0400

    Modify config/php.ini for moodle setup

--- index.php
+++ index.php
@@ -1,29 +1,22 @@
+<!DOCTYPE html>
+<html>
+<body>
+
 <?php
-$str = <<<EOD
-Example of string
-spanning multiple lines
-using heredoc syntax.
-EOD;
+$jsonobj = '{"Peter":35,"Ben":37,"Joe":43}';
+ 
+//var_dump(json_decode($jsonobj));
+$obj = json_decode($jsonobj);
 
-/* More complex example, with variables. */
-class foo
-{
-    var $foo;
-    var $bar;
+echo $obj->Peter;
+echo $obj->Ben;
+echo $obj->Joe;
+echo "<br><br>";
 
-    function __construct()
-    {
-        $this->foo = 'Foo';
-        $this->bar = array('Bar1', 'Bar2', 'Bar3');
-    }
-}
+$myArray = json_decode($jsonobj, true);
+echo $myArray['Peter'];
 
-$foo = new foo();
-$name = 'MyName';
-
-echo <<<EOT
-My name is "$name". I am printing some $foo->foo.
-Now, I am printing some {$foo->bar[1]}.
-This should print a capital 'A': \x41
-EOT;
 ?>
+
+</body>
+</html>
